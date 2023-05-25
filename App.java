4b9761995e44b5fae19a9376f2f63035a55f8252
Iris <129295205+CamiSaysUwU@users.noreply.github.com>
2023-05-24 21:11:29 -0600

    Update and rename .nda to App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+
+import veterinaria_gui.Perro;
+import veterinaria_gui.Gato;
+import veterinaria_gui.Veterinaria;
+import veterinaria_gui.ventana;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ventana ventanaP = new ventana();
+
+        ventanaP.setVisible(true); //Hacemos visible la ventana
+    }
+}
